a4a1732ed99473c9cddb270b8d081847a53ef8ed
kwonseeun <0115km@naver.com>
2022-05-04 10:52:16 +0900

    2.Hash - 전화번호 목록

---
+++ Programmers/src/Hash/example02.java
@@ -0,0 +1,40 @@
+package Hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class example02 {
+
+/*
+ * < 전화번호 목록 >  
+ * 
+전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인하려 합니다. 
+전화번호가 다음과 같을 경우, 구조대 전화번호는 영석이의 전화번호의 접두사입니다.
+
+구조대 : 119
+박준영 : 97 674 223
+지영석 : 11 9552 4421
+전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때,
+어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요. */
+	public boolean solution(String[] phone_book) {
+		boolean answer = true;
+		
+		// hashMap 선언 
+		Map<String,Integer> map = new HashMap<>();
+		
+		// 모든 전화번호를 hashMap 에 넣는다 
+		for (int i = 0; i < phone_book.length; i++) 
+			map.put(phone_book[i], i);
+
+		// 전화번호의 substring이 hashmap 에 존재하는지 확인한다. 
+		for (int i = 0; i < phone_book.length; i++) {
+			for (int j = 0; j < phone_book[i].length(); j++) {
+				if (map.containsKey(phone_book[i].substring(0, j))) {
+					return false;
+				}
+			}
+		}
+		return answer;
+	}
+	
+}
