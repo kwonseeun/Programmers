781bab3bd66c50755186dce049b398c10b95ae2d
kwonseeun <0115km@naver.com>
2022-09-18 23:43:21 +0900

    자릿수 더하기 (정수형)

--- Programmers/src/Test/P_자릿수더하기.java
+++ Programmers/src/Test/P_자릿수더하기.java
@@ -3,14 +3,14 @@ package Test;
 public class P_자릿수더하기 {
 	
 	public int solution(int n) {
-		int answer = 0;
-		String temp ="";
-		temp += n;
-		
-		for (int i = 0; i < temp.length(); i++) {
-			answer += Integer.parseInt(temp.substring(i, i+1));
-		}
-		return answer;
-	}
+        int answer = 0;
+
+        while(n != 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+
+        return answer;
+    }
 
 }
