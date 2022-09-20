b43166f096878add4bc79f406abe334499536d3c
kwonseeun <0115km@naver.com>
2022-09-20 21:49:31 +0900

    update

--- Programmers/src/Test/P_자연수뒤집어_배열로만들기.java
+++ Programmers/src/Test/P_자연수뒤집어_배열로만들기.java
@@ -6,7 +6,11 @@ public class P_자연수뒤집어_배열로만들기 {
         int lenght = num.length(); //숫자의 길이 저장
         int[] answer = new int[lenght]; //답을 저장할 배열(int)
         String[] string_array = new String[lenght]; //string배열
-        
+        for(int i = 0; i<lenght; i++) //숫자의 길이만큼 반복
+        {
+            string_array[i] = num.substring(lenght-i-1,lenght-i); //가장 끝자리의 숫자를 하나씩 저장
+            answer[i] = Integer.parseInt(string_array[i]);//string->int 형변환
+        }
         return answer;
     }
 }
