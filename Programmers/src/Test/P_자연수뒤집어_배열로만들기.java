fb1d7b2f4fce1a0f9f84f114a4387bd2e469784a
kwonseeun <0115km@naver.com>
2022-09-20 21:49:24 +0900

    update

--- Programmers/src/Test/P_자연수뒤집어_배열로만들기.java
+++ Programmers/src/Test/P_자연수뒤집어_배열로만들기.java
@@ -1,14 +1,12 @@
 package Test;
 
 public class P_자연수뒤집어_배열로만들기 {
-	 public int[] solution(long n) {
-	        String s = String.valueOf(n);
-	        int[] answer = new int[s.length()];
-	        
-	        for(int i=0; i<s.length(); i++) {
-	            int value = Integer.parseInt(s.substring(s.length()-i-1, s.length()-i));
-	            answer[i] = value;
-	        }
-	        return answer;
-	    }
+	public int[] solution(long n) {
+        String num = String.valueOf(n); //long변수에 저장된 숫자를 string으로 변환
+        int lenght = num.length(); //숫자의 길이 저장
+        int[] answer = new int[lenght]; //답을 저장할 배열(int)
+        String[] string_array = new String[lenght]; //string배열
+        
+        return answer;
+    }
 }
