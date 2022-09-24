a8e176ab72b2a8e0fac4f076e8286f1dc7b51f3b
kwonseeun <0115km@naver.com>
2022-09-24 10:52:17 +0900

    update

--- Programmers/src/Test/P_최대공약수와_최소공배수.java
+++ Programmers/src/Test/P_최대공약수와_최소공배수.java
@@ -6,6 +6,14 @@ public class P_최대공약수와_최소공배수 {
 		int temp=1;
 		int gcd = n*m;
 		
+		while (temp!=0) {
+			temp = m%n;
+			m = n;
+			n = temp;
+		}
+		
+		answer[0] = m;
+		answer[1] = gcd/m;
 		
 		return answer;
 	}
