e1e8a71047f23245dbc9b906ff8387967c996639
kwonseeun <0115km@naver.com>
2022-09-24 10:51:33 +0900

    update

---
+++ Programmers/src/Test/P_최대공약수와_최소공배수.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class P_최대공약수와_최소공배수 {
+	public int[] solution(int n, int m) {
+		int[] answer = new int[2];
+		int temp=1;
+		int gcd = n*m;
+		
+		
+		return answer;
+	}
+}
