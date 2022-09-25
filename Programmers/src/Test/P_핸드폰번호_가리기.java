3318701c6339d309e43f62e23d3827a2cad0e719
kwonseeun <0115km@naver.com>
2022-09-25 23:48:34 +0900

    update

---
+++ Programmers/src/Test/P_핸드폰번호_가리기.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class P_핸드폰번호_가리기 {
+	  public String solution(String phone_number) {
+	        char[] answerCharArray = phone_number.toCharArray();
+
+	        for (int i = 0; i < phone_number.length() - 4; i++) {
+	            answerCharArray[i] = '*';
+	        }
+	        String answer = new String(answerCharArray);
+	        return answer;
+	  }
+}
