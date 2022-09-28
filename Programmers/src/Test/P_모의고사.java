0aaae923a1b73cef6c8308fc0e16c7d4024a1385
kwonseeun <0115km@naver.com>
2022-09-28 23:56:06 +0900

    update

---
+++ Programmers/src/Test/P_모의고사.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class P_모의고사 {
+	public int[] solution(int[] answers) {
+		int[] player1 = { 1, 2, 3, 4, 5 };
+		int[] player2 = { 2, 1, 2, 3, 2, 4, 2, 5 };
+		int[] player3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+
+		int[] answer = new int[] {0,0,0};
+		int maxNum = 0;
+		
+		return answer;
+	}
+}
