b93df12044efb92281a715b13f10f5b9deacba93
kwonseeun <0115km@naver.com>
2022-10-01 12:00:36 +0900

    update

--- Programmers/src/Test/P_나누어떨어지는_숫자배열.java
+++ Programmers/src/Test/P_나누어떨어지는_숫자배열.java
@@ -17,7 +17,18 @@ public class P_나누어떨어지는_숫자배열 {
             }
         }
         
-     
+        if (count != 0) {       // 나누어 떨어지는 element가 하나라도 존재한다면
+            answer = new int[list.size()];          // answer배열의 크기는 list의 크기만큼
+            for (int i = 0; i < list.size(); i++) {
+                answer[i] = list.get(i);
+            }
+
+            Arrays.sort(answer);        // 오름차순 정렬 함수 이용
+        }
+        else {              // 나누어 떨어지는 element가 하나도 없다면
+            answer = new int[1];        // answer배열의 크기는 1만큼
+            answer[0] = -1;
+        }
         
         return answer;
     }
