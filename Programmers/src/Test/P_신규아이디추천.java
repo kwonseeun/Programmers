ca812b8cf98d25cc8b88e159fbdc69c581d43961
kwonseeun <0115km@naver.com>
2022-10-04 23:20:38 +0900

    update

--- Programmers/src/Test/P_신규아이디추천.java
+++ Programmers/src/Test/P_신규아이디추천.java
@@ -11,7 +11,7 @@ public class P_신규아이디추천 {
 				new_id=new_id.substring(1);
 			}
 			if (new_id.endsWith(".")) {
-				new_id=new_id.substring(0, new_id.length()-1)
+				new_id=new_id.substring(0, new_id.length()-1);
 			}
 			if (new_id.equals("")) {
 				new_id="a";
@@ -19,6 +19,15 @@ public class P_신규아이디추천 {
 			if (new_id.length() >= 16) {
 				new_id=new_id.substring(0,15);
 			}
+			if (new_id.endsWith(".")) {
+				new_id=new_id.substring(0, new_id.length()-1);
+			}
+			while (new_id.length()<3) {
+				new_id+=new_id+= new_id.charAt(new_id.length()-1);
+			}
+			
+			answer=new_id;
+			return answer;
 		
 	}
 }
