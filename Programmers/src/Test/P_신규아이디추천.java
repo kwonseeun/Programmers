99d9d9edca4ad6767243434f03f58b185e051b9c
kwonseeun <0115km@naver.com>
2022-10-04 23:19:00 +0900

    update

--- Programmers/src/Test/P_신규아이디추천.java
+++ Programmers/src/Test/P_신규아이디추천.java
@@ -1,5 +1,24 @@
 package Test;
 
 public class P_신규아이디추천 {
-
+	public String solution(String new_id) {
+		String answer = "";
+			new_id = new_id.toLowerCase();
+			new_id = new_id.replaceAll("[^0-9a-z.-", "");
+			new_id = new_id.replaceAll("[]+",".");
+			
+			if (new_id.startsWith(".")) {
+				new_id=new_id.substring(1);
+			}
+			if (new_id.endsWith(".")) {
+				new_id=new_id.substring(0, new_id.length()-1)
+			}
+			if (new_id.equals("")) {
+				new_id="a";
+			}
+			if (new_id.length() >= 16) {
+				new_id=new_id.substring(0,15);
+			}
+		
+	}
 }
