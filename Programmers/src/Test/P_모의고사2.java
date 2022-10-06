a4aa85be57dbfd8586a450fc2cf5e1851b78946e
kwonseeun <0115km@naver.com>
2022-10-06 23:39:20 +0900

    update

--- Programmers/src/Test/P_모의고사2.java
+++ Programmers/src/Test/P_모의고사2.java
@@ -1,5 +1,8 @@
 package Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class P_모의고사2 {
 	public int[] solution(int[] answers) {
 		
@@ -22,9 +25,15 @@ public class P_모의고사2 {
 				score[2]++;
 			}
 			
-			int topScore = Math.max(Math.max(score[0], score[1], score[2]);
+			int topScore = Math.max(Math.max(score[0], score[1]);
 			
-		return student	
+			List<Integer> students = new ArrayList<Integer>();
+			for (int j = 0; j < score.length; j++) {
+				if (topScore == score[i]) {
+					students.add(i+1);
+				}
+			}
+		return students.stream().mapToInt(Integer::intValue).toArray();	
 		}
 	}
 }
