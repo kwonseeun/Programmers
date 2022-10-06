0f651c696817877186802c5409e5efb51aaacc4d
kwonseeun <0115km@naver.com>
2022-10-06 23:38:09 +0900

    update

--- Programmers/src/Test/P_모의고사2.java
+++ Programmers/src/Test/P_모의고사2.java
@@ -8,5 +8,23 @@ public class P_모의고사2 {
 		int[] student2 = { 2,1,2,3,2,4,2,5};
 		int[] student3 = { 3,3,1,1,2,2,4,4,5,5,};
 		int[] score = new int[3];
+		
+		//채점 
+		for (int i = 0; i < answers.length; i++) {
+			int answer = answers[i];
+			if(answer == student1[i % student1.length]) {
+				score[0]++;
+			}
+			if(answer == student2[i % student1.length]) {
+				score[1]++;
+			}
+			if(answer == student3[i % student1.length]) {
+				score[2]++;
+			}
+			
+			int topScore = Math.max(Math.max(score[0], score[1], score[2]);
+			
+		return student	
+		}
 	}
 }
