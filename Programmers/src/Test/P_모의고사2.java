e92971f9269494259ee6f0c49f9815561825c18d
kwonseeun <0115km@naver.com>
2022-10-06 23:34:58 +0900

    update

---
+++ Programmers/src/Test/P_모의고사2.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class P_모의고사2 {
+	public int[] solution(int[] answers) {
+		
+		//답안지
+		int[] student1 = { 1,2,3,4,5 };
+		int[] student2 = { 2,1,2,3,2,4,2,5};
+		int[] student3 = { 3,3,1,1,2,2,4,4,5,5,};
+		int[] score = new int[3];
+	}
+}
