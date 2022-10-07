88baae31971b06d580e2149c5f491f459859a677
kwonseeun <0115km@naver.com>
2022-10-07 17:56:36 +0900

    update

--- Programmers/src/Test/P_모의고사2.java
+++ Programmers/src/Test/P_모의고사2.java
@@ -1,39 +1,22 @@
 package Test;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class P_모의고사2 {
-	public int[] solution(int[] answers) {
-		
-		//답안지
-		int[] student1 = { 1,2,3,4,5 };
-		int[] student2 = { 2,1,2,3,2,4,2,5};
-		int[] student3 = { 3,3,1,1,2,2,4,4,5,5,};
-		int[] score = new int[3];
-		
-		//채점 
-		for (int i = 0; i < answers.length; i++) {
-			int answer = answers[i];
-			if(answer == student1[i % student1.length]) {
-				score[0]++;
-			}
-			if(answer == student2[i % student1.length]) {
-				score[1]++;
-			}
-			if(answer == student3[i % student1.length]) {
-				score[2]++;
-			}
-			
-			int topScore = Math.max(score[0], score[1], score[2]);
-			
-			List<Integer> students = new ArrayList<Integer>();
-			for (int j = 0; j < score.length; j++) {
-				if (topScore == score[i]) {
-					students.add(i+1);
-				}
-			}
-		return students.stream().mapToInt(Integer::intValue).toArray();	
-		}
-	}
+	 public int[] solution(int[] answers) {
+	        int[] answer = {};
+	        int[] person1 = {1,2,3,4,5}; //이만큼씩 반복
+	        int[] person2 = {2,1,2,3,2,4,2,5};
+	        int[] person3 = {3,3,1,1,2,2,4,4,5,5};
+	        int answer1=0, answer2 =0, answer3 =0;
+	        
+	        for(int i =0; i<answers.length; i++){
+	            if(person1[i%person1.length] == answers[i]) answer1++;
+	            if(person2[i%person2.length] == answers[i]) answer2++;
+	            if(person3[i%person3.length] == answers[i]) answer3++;
+	        }
+	      
+	        
+	        return answer;
+	    }
 }
