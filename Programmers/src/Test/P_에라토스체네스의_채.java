bd4c487cd4fdee5002d53a903e5a7dbf128f80a3
kwonseeun <0115km@naver.com>
2022-10-10 23:31:22 +0900

    update

--- Programmers/src/Test/P_에라토스체네스의_채.java
+++ Programmers/src/Test/P_에라토스체네스의_채.java
@@ -1,5 +1,19 @@
 package Test;
 
 public class P_에라토스체네스의_채 {
-
+	public int solution(int n) {
+	      int answer = 1;
+	      int count =0;//소수판별 변수
+	     
+	      for(int i=3; i<=n;i++){//3부터 n까지
+	         for(int j=2; j<i; j++){
+	              if(i%j==0)
+	                  count++;
+	            }
+	          if(count==0)
+	                answer++;
+	          count=0;
+	      }
+	           return answer;
+	  }
 }
