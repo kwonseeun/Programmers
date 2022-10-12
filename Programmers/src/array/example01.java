5d1e994b225e797b33107b78758d18a12a0e086f
kwonseeun <0115km@naver.com>
2022-10-12 23:23:10 +0900

    update

---
+++ Programmers/src/array/example01.java
@@ -0,0 +1,14 @@
+package array;
+
+public class example01 {
+	public static void main(String[] args) {
+		 
+        char[] arr = new char[26];
+ 
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (char) (90 - i);
+            System.out.print(arr[i] + " ");
+        }
+ 
+    }
+}
