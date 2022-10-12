00889de1513ad2942f9ccb5521521a09cd93cec3
kwonseeun <0115km@naver.com>
2022-10-12 23:24:19 +0900

    update

---
+++ Programmers/src/array/example02.java
@@ -0,0 +1,16 @@
+package array;
+
+public class example02 {
+	public static void main(String[] args) {
+		int[] ballArr = {1,2,3,4,5,6,7,8,9};
+		int[] ball3 = new int[3];
+		// 배열 ballArr의 임의의 요소를 골라서 위치를 바꾼다.
+		for(int i=0; i< ballArr.length;i++) {
+			int j = (int)(Math.random() * ballArr.length);
+			int tmp = 0;
+			/*
+		(1) 알맞은 코드를 넣어 완성하시오.
+			 */ }
+
+	} // end of main
+}
