473ee9f662e4c375e1d086b452869189911c7820
kwonseeun <0115km@naver.com>
2022-10-16 20:50:49 +0900

    update

--- Programmers/src/array/example05.java
+++ Programmers/src/array/example05.java
@@ -12,7 +12,24 @@ public class example05 {
             System.out.println((i + 1) + "번째 데이터" + test[i]);
         }
         
-       
+        // 버블 정렬
+        for (i = 0; i < 4; i++) 
+        {
+            flag = 0;
+            for (j = 0; j < 4 - i; j++) 
+            {
+                if (test[j] < test[j + 1]) 
+                {
+                    temp = test[j];
+                    test[j] = test[j + 1];
+                    test[j + 1] = temp;
+                    flag = 1;
+                }
+            }
+ 
+           
+        }
+        
         System.out.println("=====================");
         System.out.println("=====================");
         System.out.println("정렬 후");
