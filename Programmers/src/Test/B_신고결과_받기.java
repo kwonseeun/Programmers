4eaf5d45ad7eec534dea28a70543104ec5ee80f2
kwonseeun <0115km@naver.com>
2022-11-29 23:34:46 +0900

    update

--- Programmers/src/Test/B_신고결과_받기.java
+++ Programmers/src/Test/B_신고결과_받기.java
@@ -13,7 +13,21 @@ public class B_신고결과_받기 {
 	            idxMap.put(name, i);
 	        }
 	 
-	      
+	        for (String s : report) {
+	            String[] str = s.split(" ");
+	            String from = str[0];
+	            String to = str[1];
+	            map.get(to).add(from);
+	        }
+	 
+	        for (int i = 0; i < id_list.length; i++) {
+	            HashSet<String> send = map.get(id_list[i]);
+	            if (send.size() >= k) {
+	                for (String name : send) {
+	                    answer[idxMap.get(name)]++;
+	                }
+	            }
+	        }
 	        return answer;
 	    }
 }
