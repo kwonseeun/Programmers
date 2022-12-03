d78ad4c61e08dd25f0e0580b3ed942b56c35de17
kwonseeun <0115km@naver.com>
2022-12-03 23:58:56 +0900

    update

--- Programmers/src/Test/B_체육복.java
+++ Programmers/src/Test/B_체육복.java
@@ -1,5 +1,42 @@
 package Test;
 
 public class B_체육복 {
-
+	public int solution(int n, int[] lost, int[] reserve) {
+    	int answer = 0;
+        int[] people = new int[n]; 
+        
+        //(1)
+        for(int i: lost) { 
+        	people[i-1]--;	 
+        }
+        
+        //(2)
+        for(int j: reserve) { 
+        	people[j-1]++;
+        }
+        
+        
+        for(int i=0; i<people.length;i++) {
+        	//(3)	
+        	if(people[i] == -1) { 
+        		//(4)
+                if(i>0 && people[i-1] == 1) { 
+        			people[i]++; 
+        			people[i-1]--;
+        		//(5)
+                }else if(i < people.length-1 && people[i+1] ==1) {
+        			people[i]++;
+        			people[i+1]--;
+        		}
+        	}
+        }
+        //(6)
+        for(int i : people) {
+        	if(i == 0) {
+        		answer++;
+        	}
+        }
+        
+		return answer;
+    }
 }
