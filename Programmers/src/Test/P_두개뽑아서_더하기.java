f3eb00471cbcee5d409f464c4c7e2522816adf5c
kwonseeun <0115km@naver.com>
2023-01-10 18:06:11 +0900

    update

--- Programmers/src/Test/P_두개뽑아서_더하기.java
+++ Programmers/src/Test/P_두개뽑아서_더하기.java
@@ -1,5 +1,26 @@
 package Test;
 
-public class P_두개뽑아서_더하기 {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class P_두개뽑아서_더하기 {
+	 public int[] solution(int[] numbers) {
+	        int[] answer = {};
+	        int sum = 0;
+	        ArrayList<Integer> aList = new ArrayList<Integer>();
+	        for(int i=0; i<numbers.length; i++) {
+	            for(int j=i+1; j<numbers.length; j++) {
+	                sum = numbers[i]+numbers[j];
+	                if(aList.indexOf(sum)<0) { //중복이 아니라면
+	                    aList.add(sum);
+	                }
+	            }
+	        }
+	        answer = new int[aList.size()];
+	        for(int i=0; i<aList.size(); i++) {
+	            answer[i]+=aList.get(i);
+	        }
+	        Arrays.sort(answer);
+	        return answer;
+	    }
 }
