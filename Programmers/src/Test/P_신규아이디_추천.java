d3e492cde63f9b80d2fe174335d4aa43b9e25d3c
kwonseeun <0115km@naver.com>
2023-01-25 23:21:48 +0900

    update

--- Programmers/src/Test/P_신규아이디_추천.java
+++ Programmers/src/Test/P_신규아이디_추천.java
@@ -1,5 +1,35 @@
 package Test;
 
-public class P_신규아이디_추천 {
+import java.util.ArrayList;
 
+public class P_신규아이디_추천 {
+	 public String solution(String new_id) {
+	        String answer = "";
+	        
+	        //1단계
+	        new_id = new_id.toLowerCase();
+	        
+	        //2단계
+	        String regularStr = "[^a-z0-9-_.]";
+	        new_id = new_id.replaceAll(regularStr, "");
+	 
+	        //3단계
+	        regularStr = "\\.{2,}";
+	        new_id = new_id.replaceAll(regularStr, ".");
+	        
+	        ArrayList<Character> charList = new ArrayList<>();
+	        
+	        for(int i=0; i<new_id.length(); i++) {
+	            charList.add(new_id.charAt(i));
+	        }
+	        
+	        //4단계        
+	        if (charList.size() > 0) {
+	            if(charList.get(0) == '.' ) charList.remove(0);
+	            if(charList.size() > 0 && '.' == charList.get(charList.size()-1) ) 
+	            	charList.remove(charList.size()-1);
+	        }
+			return regularStr;
+	        
+	 }
 }
