bc478118aa817f5f9f3470b5b653c5ab1fd6c084
kwonseeun <0115km@naver.com>
2023-02-15 22:33:53 +0900

    프로그래머스 모의고사

---
+++ Programmers/src/Test/P_모의고사_2.java
@@ -0,0 +1,30 @@
+package Test;
+
+import java.util.ArrayList;
+
+public class P_모의고사_2 {
+	public int[] solution(int[] answers) {
+        int[] answer = new int[] {0,0,0};
+        int[] player1 = {1,2,3,4,5};
+        int[] player2 = {2,1,2,3,2,4,2,5};
+        int[] player3 = {3,3,1,1,2,2,4,4,5,5};
+        int max = 0;
+        ArrayList<Integer> countNum = new ArrayList<>();
+
+
+        for(int i=0; i<answers.length; i++){
+            if(answers[i] == player1[i%5]){
+                answer[0]++;
+            }
+            if(answers[i] == player2[i%8]){
+                answer[1]++;
+            }
+            if(answers[i] == player3[i%10]){
+                answer[2]++;
+            }
+        }
+
+      
+        return countNum.stream().mapToInt(i->i).toArray();
+    }
+}
